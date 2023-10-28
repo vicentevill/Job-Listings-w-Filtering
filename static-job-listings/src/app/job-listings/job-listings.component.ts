f6bcd9a3678e9a6e19ec40792b691da8ff5c99ff
thekingvice <villarrealvicente92@gmail.com>
2023-10-27 20:53:36 -0700

    commit

--- static-job-listings/src/app/job-listings/job-listings.component.ts
+++ static-job-listings/src/app/job-listings/job-listings.component.ts
@@ -7,11 +7,48 @@ import { FetchJobListingsService } from '../fetch-job-listings.service';
   styleUrls: ['./job-listings.component.css'],
 })
 export class JobListingsComponent {
-  jobListingsData: any;
+  jobListingsData: any[] = [];
+
+  filteredJobListings: any[] = [];
+
+  languageFilters = ['JavaScript'];
+
+  toolsFilters = ['Sass'];
+
+  // filterListings() {
+  //   this.filteredJobListings = this.jobListingsData.filter((listing) => {
+  //     return this.languageFilters.every((filter) =>
+  //       listing.languages.includes(filter)
+  //     );
+  //   });
+  // }
+  filterListings() {
+    this.filteredJobListings = this.jobListingsData.filter((listing) => {
+      // Filter by languages
+      const languageFilterPass = this.languageFilters.every((language) =>
+        listing.languages.includes(language)
+      );
+
+      // Filter by tools
+      const toolsFilterPass = this.toolsFilters.every((tool) =>
+        listing.tools.includes(tool)
+      );
+
+      // Return true if both language and tools filters pass
+      return languageFilterPass && toolsFilterPass;
+    });
+  }
+  //  testData = [
+  //     { lang: ['JS', 'TS'] },
+  //     { lang: ['JS', 'TS', 'HTML'] },
+  //     { lang: ['HTML', 'CSS'] },
+  //   ];
   constructor(private fetchJobListingsService: FetchJobListingsService) {}
+
   ngOnInit(): void {
     this.fetchJobListingsService.getData().subscribe((result) => {
       this.jobListingsData = result;
+      this.filteredJobListings = result;
     });
   }
 }
